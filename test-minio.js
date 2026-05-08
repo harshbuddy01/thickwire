9ac7622fa1db94e9b2482408e590e9f6080dae72
HARSH ANAND <harshanand@HARSHs-MacBook-Air.local>
2026-05-09 03:23:47 +0530

    chore: remove 37 one-off utility scripts and harden .gitignore

--- test-minio.js
+++
@@ -1,4 +0,0 @@
-const axios = require('axios');
-axios.get('https://bucket-production-6fef.up.railway.app/streamkart-assets')
-  .then(res => console.log(res.data))
-  .catch(err => console.error(err));
